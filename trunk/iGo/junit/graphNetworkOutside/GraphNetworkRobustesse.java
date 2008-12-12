cc117b622b22663802fb32f9b8bac460fc8d2ee8
Icarius44 <Icarius44@eaef3ece-8374-11dd-90a9-6306d3ffb32f>
2008-12-12 14:32:31 +0000

    mise à jours des tests de robustesses

--- trunk/iGo/junit/graphNetworkOutside/GraphNetworkRobustesse.java
+++ trunk/iGo/junit/graphNetworkOutside/GraphNetworkRobustesse.java
@@ -1,7 +1,9 @@
 package graphNetworkOutside;
 
+import static org.junit.Assert.assertTrue;
 import graphNetwork.GraphNetwork;
 import graphNetwork.GraphNetworkBuilder;
+import graphNetwork.exception.ViolationOfUnicityInIdentificationException;
 import junit.framework.JUnit4TestAdapter;
 
 import org.junit.After;
@@ -25,9 +27,69 @@ public class GraphNetworkRobustesse {
 	public static junit.framework.Test suite() {
 		return new JUnit4TestAdapter(GraphNetworkRobustesse.class);
 	}
-	
+
 	@Test
-	public void getJunction(){
-		
+	public void getJunction() throws ViolationOfUnicityInIdentificationException {
+		assertTrue("Doit retourné null car une des stations est null", sncf.getJunctions(null, null) == null);
+		bob.addService(1, "e");
+		assertTrue("Doit retourné null car une des stations est null",
+				sncf.getJunctions(sncf.getStation(1), null) == null);
+		assertTrue("Doit retourné null car une des stations est null",
+				sncf.getJunctions(null, sncf.getStation(1)) == null);
+		bob.addService(2, "e");
+		assertTrue("Doit retourné un iterateur sans element car aucun lien entre les stations", !sncf.getJunctions(
+				sncf.getStation(2), sncf.getStation(1)).hasNext());
+	}
+
+	@Test
+	public void getEntryCost() {
+		assertTrue("Doit retourné Float.Nan car une le kind est null", sncf.getEntryCost(null) == Float.NaN);
+	}
+
+	@Test
+	public void getInstancePathInGraphBuilder() {
+		assertTrue("On ne peut construire un PathInGraphBuilder pour un path null", sncf
+				.getInstancePathInGraphBuilder(null) == null);
+	}
+
+	@Test
+	public void getKindFromString() {
+		assertTrue("le kindString est null, le retour doit aussi l'être", null == sncf.getKindFromString(null));
+	}
+
+	@Test
+	public void getRoute() {
+		assertTrue("le nom est null, le retour doit l'être", null == sncf.getRoute(null));
+		assertTrue("le nom de Route est inconnu, le retour doit être null", null == sncf.getRoute("ee"));
+	}
+
+	@Test
+	public void getService() {
+		assertTrue("le num de Service est incounn, le retour doit être null", null == sncf.getService(-23));
+	}
+
+	@Test
+	public void getStation() {
+		assertTrue("le num de Station est incounn, le retour doit être null", null == sncf.getStation(-42));
+	}
+
+	@Test
+	public void getKinds() {
+		assertTrue("l'iterateur doit être vide", !sncf.getKinds().hasNext());
+	}
+
+	@Test
+	public void getRoutes() {
+		assertTrue("l'iterateur doit être vide", !sncf.getRoutes().hasNext());
+	}
+
+	@Test
+	public void getServices() {
+		assertTrue("l'iterateur doit être vide", !sncf.getServices().hasNext());
+	}
+
+	@Test
+	public void getStations() {
+		assertTrue("l'iterateur doit être vide", !sncf.getStations().hasNext());
 	}
 }
